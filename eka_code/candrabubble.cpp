b996f8d68fa57f15a8af924131bb4bc9fd993719
GovalPrabawa <imadegoval726@gmail.com>
2023-01-22 19:22:24 +0800

    mengubah kode pada baris 20

--- eka_code/candrabubble.cpp
+++ eka_code/candrabubble.cpp
@@ -1,31 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int a, arr[5], num, first, last, middle;
-    cout<<"Enter 5 Elements (in ascending order): ";
-    for(a=0; a<5; a++)
-        cin>>arr[a];
-    cout<<"\nEnter Element to be Search: ";
-    cin>>num;
+    cout << "Enter 5 Elements (in ascending order): ";
+    for (a = 0; a < 5; a++)
+        cin >> arr[a];
+    cout << "\nEnter Element to be Search: ";
+    cin >> num;
     first = 0;
     last = 9;
-    middle = (first+last)/2;
-    while(first <= last)
+    middle = (first + last) / 2;
+    while (first <= last)
     {
-        if(arr[middle]<num)
-            first = middle+1;
-        else if(arr[middle]==num)
+        if (arr[middle] < num)
+            first = middle + 1;
+        else if (arr[middle] == num)
         {
-            cout<<"\nThe number, "<<num<<" found at Position "<<middle+1;
+            cout << "\nThe number, " << num << " Data ditemukan pada posisi " << middle + 1;
             break;
         }
         else
-            last = middle-1;
-        middle = (first+last)/2;
+            last = middle - 1;
+        middle = (first + last) / 2;
     }
-    if(first>last)
-        cout<<"\nThe number, "<<num<<" is not found in given Array";
-    cout<<endl;
+    if (first > last)
+        cout << "\nThe number, " << num << " is not found in given Array";
+    cout << endl;
     return 0;
 }
