dbd3880532e9b3473249fd58e111baa4f233b8e5
GovalPrabawa <imadegoval726@gmail.com>
2023-03-23 21:40:32 +0800

    .

---
+++ kdp/cek_bilangan_prima.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    bool isPrime = true;
+
+    cout << "Masukkan sebuah bilangan bulat positif: ";
+    cin >> num;
+
+    if (num <= 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+    {
+        cout << num << " adalah bilangan prima" << endl;
+    }
+    else
+    {
+        cout << num << " bukan bilangan prima" << endl;
+    }
+
+    return 0;
+}
--- kdp/soal2.cpp
+++ kdp/soal2.cpp
@@ -1,85 +1,13 @@
 #include <iostream>
-#include <conio.h>
+
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-    int persegi, i, j, n;
-    int spasi, baris;
-    char ganjil[15] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29};
-    char genap[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-    char prima[9] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+    int a = 7, b = 2;
+    int total = 0;
 
-    int pilihan;
-    cout << "Menu Pilihan : " << endl;
-    cout << "1. Kotak " << endl;
-    cout << "2. Segitiga " << endl;
-    cout << "3. Exit " << endl;
-    cout << "masukan pilihan 1/2/3 : ";
-    cin >> pilihan;
-    cout << endl;
-
-    int mode;
-    if (pilihan == 1)
-    {
-        cout << " MODE " << endl;
-        cout << "1. Ganjil (1-30)" << endl;
-        cout << "2. Genap (2-20)" << endl;
-        cout << "3. Prima (2-27))" << endl;
-        cout << "Pilih Mode Yang Diinginkan :";
-        cin >> mode;
-        cout << endl;
-    }
-
-    switch (pilihan)
-    {
-    case 1:
-    {
-        cout << "Masukkan panjang sisi persegi: ";
-        cin >> n;
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (mode == 1)
-                {
-                    cout << "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29";
-                }
-                else if (mode == 2)
-                {
-                    cout << "2,4,6,8,10,12,14,16,18,20";
-                }
-                else if (mode == 3)
-                {
-                    cout << "2,3,5,7,11,13,17,19,23";
-                }
-                cout << endl;
-            }
-        }
-        break;
-    }
-    case 2:
-    {
-        cout << "Masukan Jumlah Baris: ";
-        cin >> baris;
-
-        for (int i = 1, k = 0; i <= baris; ++i, k = 0)
-        {
-            for (spasi = 1; spasi <= baris - i; ++spasi)
-            {
-                cout << "  ";
-            }
-
-            while (k != 2 * i - 1)
-            {
-                cout << " ";
-                ++k;
-            }
-            cout << endl;
-        }
-        break;
-    }
-    }
+    total = a / b;
+    cout << total << endl;
     return 0;
 }
